42ce90edd5021ded574d2ab85d48f5eaa44fa552
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-12-13 22:52:02 +0700

    logout button and function

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,19 +18,27 @@
         left
         bottom
       >
-        <template v-slot:activator="{ on }">
-          <v-btn icon v-on="on" v-if="isLogin">
-            <v-icon>mdi-account</v-icon>
+        <template v-if="isLogin" v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-avatar height="hole">
+              <img
+                src="https://cdn.vuetifyjs.com/images/john.jpg"
+                alt="John"
+              >
+            </v-avatar>
           </v-btn>
         </template>
 
         <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          <v-list-item>
+            <v-list-item-title style="width: 200px">
+              <v-row>
+                <v-icon>mdi-account</v-icon>View Profile
+              </v-row>
+              <v-row>
+                <v-icon @click="logout">mdi-logout</v-icon>Sign Out
+              </v-row>
+            </v-list-item-title>
           </v-list-item>
         </v-list>
       </v-menu>
@@ -43,6 +51,13 @@ export default {
   name: 'Navbar',
   computed: {
     ...mapState(['isLogin'])
+  },
+  methods: {
+    logout () {
+      localStorage.removeItem('token')
+      this.$store.commit('SET_IS_LOGIN', false)
+      this.$router.push('/signin')
+    }
   }
 }
 
